d035a578f3c30be8a569044c930f067a5681b6a3
DaanishAli <danish79786@gmail.com>
2022-11-16 15:25:39 +0500

    basic-redux-toolkit

---
+++ src/features/counter/Counter.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment, incrementByAmount, reset } from './counterSlice'
+
+export function Counter() {
+    const count = useSelector((state) => state.counter.value)
+    const dispatch = useDispatch()
+
+    return (
+        <div>
+            <div>
+                <button
+                    aria-label="Increment value"
+                    onClick={() => dispatch(increment())}
+                >
+                    Increment
+                </button>
+                <span>{count}</span>
+                <button
+                    aria-label="Decrement value"
+                    onClick={() => dispatch(decrement())}
+                >
+                    Decrement
+                </button>
+                <button
+                    aria-label="Decrement value"
+                    onClick={() => dispatch(incrementByAmount(2))}
+                >
+                    increment with 2
+                </button>
+                <button
+                    aria-label="Decrement value"
+                    onClick={() => dispatch(reset())}
+                >
+                    reset
+                </button>
+            </div>
+        </div>
+    )
+}
